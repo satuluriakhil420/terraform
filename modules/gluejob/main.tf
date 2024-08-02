c133022e148d54669c58b9ab98466a146d60867c
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-02 13:15:01 +0530

    Update main.tf

--- modules/gluejob/main.tf
+++ modules/gluejob/main.tf
@@ -1,4 +1,4 @@
-resource "aws_glue_job" "example" {
+resource "aws_glue_job" "glue_job" {
   count                     = length(var.glue_job_script_locations)
   name                      = element(var.glue_job_script_locations, count.index)
   role_arn                  = var.iam_role_arn
@@ -8,6 +8,5 @@ resource "aws_glue_job" "example" {
 }
 
 output "glue_job_names" {
-  value = aws_glue_job.example[*].name
+  value = aws_glue_job.glue_job[*].name
 }
-
