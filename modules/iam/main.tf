6d0b1fd15dd143b7a7e1f82cefa3ab18747f6b7c
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-02 13:15:51 +0530

    Update main.tf

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_role" "myrole" {
+resource "aws_iam_role" "sentrics_role" {
   name       = var.rolename
 
   assume_role_policy = <<EOF
@@ -18,36 +18,36 @@ resource "aws_iam_role" "myrole" {
 EOF
 }
 resource "aws_iam_role_policy_attachment" "s3_role_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "glue_role_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
 resource "aws_iam_role_policy_attachment" "gluenotebook_policy_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceNotebookRole"
 }
 
 resource "aws_iam_role_policy_attachment" "glueconsole_policy_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSGlueConsoleFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "service_catalog_policy_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/AWSServiceCatalogAdminFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "athena_policy_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonAthenaFullAccess"
 }
 
 resource "aws_iam_role_policy_attachment" "quicksight_policy_attachment" {
-  role       = aws_iam_role.myrole.name
+  role       = aws_iam_role.sentrics_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSQuicksightAthenaAccess"
 }
 
