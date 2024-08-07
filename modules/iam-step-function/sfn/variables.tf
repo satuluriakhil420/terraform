056b8fe269bb1d7c316e83b134367559293c2c73
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-07 11:02:35 +0530

    Update variables.tf

--- modules/iam-step-function/sfn/variables.tf
+++ modules/iam-step-function/sfn/variables.tf
@@ -1,7 +1,12 @@
-variable "state_machine_name" {
-  description = "Name of the Step Functions state machine"
+variable "state_machines" {
+  description = "List of state machines"
+  type = list(object({
+    name       = string
+    definition = string
+  }))
 }
 
 variable "role_arn" {
   description = "ARN of the IAM role for Step Functions"
+  type        = string
 }
