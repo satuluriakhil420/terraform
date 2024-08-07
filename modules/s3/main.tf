a5de38acf73fe7919c635e8252d95c3aad2008db
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-07 15:06:52 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -7,7 +7,7 @@ data "aws_s3_bucket" "existing" {
   bucket = var.bucketname
 }
 
-resource "aws_s3_bucket_ownership_controls" "ws_s3_bucket_ownership_controls" {
+resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
   depends_on = [aws_s3_bucket.s3_bucket]
 
   bucket = data.aws_s3_bucket.existing.id
