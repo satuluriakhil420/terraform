0e254ba0f6c3b29475a55d327c57c39591e48711
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-07 15:37:49 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -3,14 +3,10 @@ resource "aws_s3_bucket" "s3_bucket" {
   acl    = "private"
 }
 
-data "aws_s3_bucket" "existing" {
-  bucket = var.bucketname
-}
-
 resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
   depends_on = [aws_s3_bucket.s3_bucket]
 
-  bucket = data.aws_s3_bucket.existing.id
+  bucket = var.bucketname
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -22,6 +18,6 @@ resource "aws_s3_bucket_acl" "aws_s3_bucket_acl" {
     aws_s3_bucket_ownership_controls.aws_s3_bucket_ownership_controls
   ]
 
-  bucket = data.aws_s3_bucket.existing.id
+  bucket = var.bucketname
   acl    = "private"
 }
