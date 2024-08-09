3349caf9577ea9d70e4da4f890ad936f85483720
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-09 12:30:52 +0530

    Update main.tf

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
   bucket = var.bucketname
   acl    = "private"
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
@@ -11,6 +14,9 @@ resource "aws_s3_bucket_ownership_controls" "aws_s3_bucket_ownership_controls" {
   rule {
     object_ownership = "BucketOwnerPreferred"
   }
+  lifecycle {
+    ignore_changes = all
+  }
 }
 
 resource "aws_s3_bucket_acl" "aws_s3_bucket_acl" {
@@ -19,8 +25,9 @@ resource "aws_s3_bucket_acl" "aws_s3_bucket_acl" {
   ]
 
   bucket = var.bucketname
-  acl    = "private"
+  acl    = "private
+  lifecycle {
+    ignore_changes = all
+  }
 }
-lifecycle {
-  ignore_changes = all
-}
+
