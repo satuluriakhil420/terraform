a9a94dbff0954c836c09972401639cc4dce43019
satuluriakhil420 <94843341+satuluriakhil420@users.noreply.github.com>
2024-08-09 12:02:13 +0530

    Update main.tf

--- modules/iam-step-function/iam-sfn/main.tf
+++ modules/iam-step-function/iam-sfn/main.tf
@@ -39,3 +39,6 @@ resource "aws_iam_role_policy_attachment" "attach_step_function_policy" {
   role       = aws_iam_role.step_function_role.name
   policy_arn = aws_iam_policy.step_function_policy.arn
 }
+lifecycle {
+  ignore_changes = all
+}
